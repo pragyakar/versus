b005ba85299868d5d6aebd44a0b98bc76fbfd1fc
Pragyakar Joshi <pragyakar75@gmail.com>
2018-08-03 15:10:50 +0545

    Additional Styling

--- js/app.js
+++ js/app.js
@@ -5,28 +5,32 @@ var totalVotes = pointA + pointB;
 function addleft(){
     pointA += 1;
     totalVotes += 1;
-    updateVotes();
+    updatePoints();
     console.log(pointA + ' '+ pointB);
 }
 function addright(){
     pointB += 1;
     totalVotes += 1;
-    updateVotes();
+    updatePoints();
     console.log(pointA + ' '+ pointB);
 }
 
-function updateVotes(){
-    document.getElementById("voting-box").style.gridTemplateColumns= getSize();
-}
-
-function getSize(){
+function updatePoints(){
     var percentA = (pointA / totalVotes) * 100;
     var percentB = (pointB / totalVotes) * 100;
+    document.getElementById("size-one").innerHTML = Math.round(percentA) + '%';
+    document.getElementById("size-two").innerHTML = Math.round(percentB) + '%';
     var size = percentA + "% " + percentB + "%";
     console.log(size);
+    document.getElementById("voting-box").style.gridTemplateColumns=  percentA + "% " + percentB + "%";
     return size;
 }
 
-function reset() {
+function setup() {
+    pointA = 1;
+    pointB = 1;
+    totalVotes = pointA + pointB;
+    document.getElementById("option-one").innerHTML = "Option A";
+    document.getElementById("option-two").innerHTML = "Option B";
     document.getElementById("voting-box").style.gridTemplateColumns= "50% 50%";
 }
