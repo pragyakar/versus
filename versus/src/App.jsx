d039c342825473dc715e0161232e5fdd99001360
pragyakar <pragyakar75@gmail.com>
2019-03-31 21:09:12 +0545

    :art: Added font and basic CSS

--- versus/src/App.jsx
+++ versus/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Landing from './components/Landing';
 import './App.css';
 
 const App = () => (    
-  <div className="App"></div>
+  <div className="App">
+    <Landing/>
+  </div>
 );
 
 export default App;
