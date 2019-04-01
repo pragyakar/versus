576b7f6665eb065fd47e6ccb87a56119b4897858
pragyakar <pragyakar75@gmail.com>
2019-04-01 22:39:23 +0545

    Updates

---
+++ precache-manifest.f4d1270c8981bb158b74da870da430b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aaa0e4f2a00de0408065",
+    "url": "/versus/static/css/main.d9c1e87e.chunk.css"
+  },
+  {
+    "revision": "aaa0e4f2a00de0408065",
+    "url": "/versus/static/js/main.4e59f206.chunk.js"
+  },
+  {
+    "revision": "da89e33858b9f5e63fea",
+    "url": "/versus/static/js/runtime~main.d14c347d.js"
+  },
+  {
+    "revision": "24852345734ce1d91ff0",
+    "url": "/versus/static/js/2.5bb7d9f6.chunk.js"
+  },
+  {
+    "revision": "022465beb043134bfd8fcd6d0bc3c255",
+    "url": "/versus/static/media/bg.022465be.jpg"
+  },
+  {
+    "revision": "946ad5a97c1c61ae66b088e94a8b5afa",
+    "url": "/versus/index.html"
+  }
+];
